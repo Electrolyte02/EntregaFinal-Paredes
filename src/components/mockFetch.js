480813d10ba29cd3001de274376eaf84bdb10c60
Esteban Paredes <estebanjparedes@hotmail.com>
2023-04-10 18:55:40 -0300

    Eliminacion archivo innecesario

--- src/components/mockFetch.js
+++
@@ -1,6 +0,0 @@
- export const getProduct = (id) =>{
-    const promise = new Promise((resolve,reject) =>{
-        resolve(id ? prod.find((producto)=> producto.id == id ):prod)
-    });
-    return promise;
- };
